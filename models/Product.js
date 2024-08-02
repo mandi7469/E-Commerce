bf94108a8fcee70dc136f71a5032c3f68771fea1
mandi7469 <mandi7469@aol.com>
2024-08-02 16:47:33 -0400

    defined the tag columns

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // defined product columns
+    // defined the product columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
