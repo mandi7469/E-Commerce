120636c67d96f77f2d73bd36f185097a0dd07572
mandi7469 <mandi7469@aol.com>
2024-08-06 15:36:08 -0400

    final touches

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     foreignKey: "product_id",
-    // unique: false,
+  
   },
 });
 
@@ -30,7 +30,7 @@ Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     foreignKey: "tag_id",
-    // unique: false,
+    
   },
 });
 
